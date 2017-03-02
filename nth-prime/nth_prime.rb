5b14cf355cdb130963006b9dabe54879e649f771
Leandro Martins <leandromarte@gmail.com>
2017-03-02 07:51:05 -0300

    Nth-prime

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,24 +1,22 @@
 class Prime
   def self.nth n
-    raise ArgumentError if n == 0
+    raise ArgumentError.new "Number can't be negative or 0. Insert a positive integer." if n <= 0
 
-    primes = [2]
+    last_prime = 2
+    primes_count = 1
     i = 3
-    while (primes.size < n) do
+    while (primes_count < n) do
       if (prime? i)  
-        primes.push i
+        last_prime = i
+        primes_count += 1
       end
-      i += 1
-
-      i += 1 if (i % 2 == 0)
-      i += 1 if (i % 3 == 0)   
+      i += 1        
     end
-    return primes.last
+    last_prime
   end
-
-  private 
+ 
   def self.prime? number
-    return ((1 .. number/2).collect{ |n| [n, number / n] if ((number / n) * n) == number }.compact.size == 1)
+    (2 .. number/2).none? { |n| number % n == 0}
   end
 end
 
